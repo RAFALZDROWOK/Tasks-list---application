6cdc06b5edf104de3e9cc6a48ae69ee68df9d883
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-03-08 14:01:20 +0100

    added function related to removing a task from the list of tasks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 {
     const tasks = [];
 
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
