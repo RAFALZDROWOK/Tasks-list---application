6ed58d74912ac95722b7a2431bc56c94578b969e
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-03-08 13:29:18 +0100

    creating the "render" function, modifying the content in HTML

--- js/script.js
+++ js/script.js
@@ -1,6 +1,44 @@
 {
     const tasks = [];
 
+    const render = () => {
+        let taskListHTMLContent = "";
+
+        for (const task of tasks) {
+            taskListHTMLContent += `
+        <li class="tasks__item js-task">
+
+            <button class="tasks__button tasks__button--toggleDone js-toogleDone">
+              ${task.done ? "✔" : ""}
+            </button>
+
+            <span class="tasks__content${ task.done ? " tasks__content--done" : ""}">
+              ${task.content}
+            </span>
+
+            <button class="tasks__button tasks__button--remove js-remove">
+              🗑
+            </button>
+
+        </li>
+        `;
+        } 
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskElement = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskElement.value.trim();
+        
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskElement.value = "";
+        }
+
+        newTaskElement.focus();
+    };
+
     const init = () => {
         render();
 
