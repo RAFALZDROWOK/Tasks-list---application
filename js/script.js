15833124f12fa73255bdbf06f7773e198bdd5d50
Rafał Zdrowok <r.zdrowok.sne@gmail.com>
2023-04-26 11:26:23 +0200

    Introduction of the renderTasks function

--- js/script.js
+++ js/script.js
@@ -46,6 +46,27 @@
         });
     };
 
+    const renderTasks = () => {
+        const taskToHTML = task => `
+            <li class="
+                tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""} js-task">
+                <button class="tasks__button tasks__button--toggleDone js-toogleDone">
+                    ${task.done ? "✔" : ""}
+                </button>
+                <span class="tasks__content${ task.done ? " tasks__content--done" : ""}">
+                    ${task.content}
+                </span>
+                <button class="tasks__button tasks__button--remove js-remove">
+                 🗑
+                </button>
+            </li>
+            `;
+        
+        const tasksElement = document.querySelector(".js-tasks");
+        tasksElement.innerHTML = tasks.map(taskToHTML).join("");
+        
+    };
+
     const render = () => {
         renderTasks()
         bindRemoveEvents();
